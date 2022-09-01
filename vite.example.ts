df48c683044e5382a5f15d1b3573ddadc86663af
Phong Nguyen <phongnn@gmail.com>
2022-09-01 11:44:31 +0700

    Update config files

--- vite.example.ts
+++ vite.example.ts
@@ -1,8 +1,15 @@
+import path from "node:path";
 import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 
 export default defineConfig(() => {
   return {
+    root: "example",
+    resolve: {
+      alias: {
+        "qwik-lib-starter": path.resolve("./src/index.ts"),
+      },
+    },
     plugins: [qwikVite()],
   };
 });
