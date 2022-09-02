1e581acbe9cec9e9d662b3088d5b739d4cfb8c1f
Phong Nguyen <phongnn@gmail.com>
2022-09-02 12:43:25 +0700

    feat: add multiply

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,13 @@
-import { add } from ".";
+import { add, multiply } from ".";
 
 describe("add", () => {
   it("should work", () => {
     expect(add(1, 1)).toBe(2);
+  });
+});
+
+describe("multiply", () => {
+  it("should work", () => {
+    expect(multiply(3, 2)).toBe(6);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,7 @@ export function add(a: number, b: number) {
 export function subtract(a: number, b: number) {
   return a - b;
 }
+
+export function multiply(a: number, b: number) {
+  return a * b;
+}
